62dcee7717147e5c2dd20f56ab8c3d55ca8c8a2d
Matthew Brett <matthew.brett@gmail.com>
2010-05-17 22:28:22 -0700

    BF - trying git fancy footwork to make new dev version number

--- nipy/info.py
+++ nipy/info.py
@@ -2,7 +2,8 @@
 
 This script should do no imports.  It only defines variables.
 '''
-
+version = '0.1.2'
+is_release = False
 
 long_description = \
 """
--- nipy/version.py
+++ nipy/version.py
@@ -1,29 +1,40 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-version='0.1.2'
-release=False
 
-if not release:
-    have_revno = False
+# This file needs to be imported within numpy distutils, before nipy has
+# been built or installed.  It can't use any nipy components or relative
+# imports
+import os
+_THIS_DIR = os.path.dirname(__file__)
+
+# Please do not change these lines to use execfile because execfile is
+# not available in Python 3
+INFO_VARS = {}
+exec(open(os.path.join(_THIS_DIR, 'info.py'), 'rt').read(), {}, INFO_VARS)
+
+version = INFO_VARS['version']
+
+if not INFO_VARS['is_release']:
+    suffix = '.dev'
+    import subprocess
+    cmd = 'git log --max-count=1 --date=short --format="%cd#%h"'
     try:
-        import os
-        import subprocess
-        # Use bzr to get the revision number.  Execute bzr in the same
+        # Use git to get the revision number.  Execute git in the same
         # directory as this file.
-        dir = os.path.dirname(__file__)
-        proc = subprocess.Popen(['bzr', 'revno'], stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                cwd=dir)
-        revno, errcode = proc.communicate()
-        if revno:
-            revno = int(revno)
-            version += 'dev%d' % revno
-            have_revno = True
+        proc = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+            cwd=_THIS_DIR)
     except OSError:
-        # Either bzr was not found or nipy was imported from a
-        # directory that isn't a bzr branch.  Just ignore.
+        # Either git was not found or nipy was imported from a
+        # directory that isn't a git branch.  Just ignore.
         pass
-    finally:
-        if not have_revno:
-            # just tack on a dev label
-            version += '.dev'
+    else:
+        rev_str, err = proc.communicate()
+        if not rev_str == '':
+            rev_str = rev_str.strip().replace('-', '')
+            rev_str = rev_str.replace('#', '-')
+            suffix += rev_str
+    version += suffix

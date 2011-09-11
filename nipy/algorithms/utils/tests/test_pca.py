3a6cfb7cb531eb23598dc9c33b34663c15e65df0
Matthew Brett <matthew.brett@gmail.com>
2011-09-10 22:10:20 -0700

    RF - moved PCA into algorithms.utils
    PCA was in modalilities.fmri, but the routine is general

--- nipy/algorithms/utils/tests/test_pca.py
+++ nipy/algorithms/utils/tests/test_pca.py
@@ -2,8 +2,9 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 import numpy as np
 
-from nipy.modalities.fmri.pca import pca
+from ..pca import pca
 from nipy.io.api import  load_image
+
 from nipy.testing import assert_equal, assert_almost_equal, \
     assert_array_almost_equal, funcfile, assert_true, \
     assert_array_equal, assert_raises, \
